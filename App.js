ae4c16592bb639cf8ba924768291e95f72426dd8
emilianostic <emilianostic@gmail.com>
2024-06-13 16:25:31 -0300

    coloco Text, Image, View y Stylesheet

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Text, View, StyleSheet, Image } from "react-native"; //etiqueta <p> o similar
+//View = DIV, Image Textinput ScrollView Stylsheet
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Hola</Text>
+      <Image source={{uri:"https://picsum.photos/200/200"}}
+      style={styles.image}/>
     </View>
   );
-}
-
+};
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  container: { flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "#292929"  },
+  title: { fontSize: 30, color: "#fff" },
+  image: {height:200, width: 200}
 });
+export default App;
